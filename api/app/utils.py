00ddd74dc782d37cba4c6c0f252be9b3f2b5afd7
Mark Pare <mark.p.pare@gmail.com>
2020-12-13 05:15:02 -0800

    Fix deploy

--- api/app/utils.py
+++ api/app/utils.py
@@ -45,7 +45,6 @@ def get_prediction(image_bytes):
     main_model = get_model()
     tensor = transform_image(image_bytes)
     outputs = main_model(tensor)
-    print('outputs ' + outputs)
     # full_results = {}
     # for index, key in enumerate(class_names):
     #     full_results[key] = outputs[]
